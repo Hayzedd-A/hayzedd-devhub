5afe8b09e87d9da13cf754942a73becd1634e520
hayzedd-a <adebayoazeez37@yahoo.com>
2026-02-03 09:52:10 +0100

    still in progress

---
+++ info.js
@@ -0,0 +1,17 @@
+const myInfo = {
+    brandName: "Hayzedd",
+    fullname: "Adebayo Azeez",
+    location: {
+        country: "Nigeria",
+        state: "Lagos",
+        "city": "Epe",
+    },
+    stacks: ["React", "Express", "node", "Next"], // I'm a full stack web developer, add all neccessary skill including databases
+    portfoioWebsite: "https://hayzedd-the-developer.vercel.app",
+    contact: {
+        phone: '08081602424',
+        email: "adebayoazeez37@yahoo.com",
+        linkendIn: "https://www.linkedin.com/in/azeez-adebayo-ola",
+        github: "https://github.com/hayzedd-a"
+    }
+}
